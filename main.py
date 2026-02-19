f57088af0c9e64e70ae4e9bb2e2a9ee8e83a66f9
BenUsmane <syllaousmane279@gmail.com>
2026-02-20 06:11:03 +0800

    plotting accuracy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from model.model import MyModel
 from model.train import train
 from torch import nn, optim
 from data.data import train_loader, val_loader
+import matplotlib.pyplot as plt
 
 
 model = MyModel()
@@ -10,6 +11,22 @@ loss_function = nn.BCEWithLogitsLoss()
 opt = optim.Adam(model.parameters(), lr = 0.001)
 
 
-epochs = 100
+epochs = 50
 if __name__ == "__main__":
-    train(epochs,model,loss_function, opt,train_loader,val_loader)
+    accu_train_loss, accu_val_loss, accu_train_accuracy, accu_val_accuracy, accu_train_precision, accu_val_precision, accu_iteration, model =train(
+        epochs,
+        model,
+        loss_function,
+        opt,
+        train_loader,
+        val_loader
+    )
+plt.plot(accu_iteration, accu_train_accuracy, c='red')
+plt.plot(accu_iteration, accu_val_accuracy, c='green')
+
+plt.legend(['train accuracy', 'val accuracy'])
+plt.xlabel('iteration')
+plt.ylabel('accuracy')
+
+
+plt.show()
