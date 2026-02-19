74fce8704ac18e0c99d8d8d4e406bd39d17b611c
BenUsmane <syllaousmane279@gmail.com>
2026-02-20 01:31:24 +0800

    data preprocessing file

--- main.py
+++ main.py
@@ -14,36 +14,6 @@ from sklearn.metrics import accuracy_score, precision_score, confusion_matrix
 from seaborn import heatmap
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-feature_name = load_breast_cancer().feature_names
-load_breast_cancer().target_names
-data = load_breast_cancer().data
-target = load_breast_cancer().target
-X, y = load_breast_cancer(return_X_y=True)
-scale = StandardScaler()
-
-X_tensor = torch.from_numpy(X).type(torch.float32)
-y_tensor = torch.from_numpy(y).type(torch.float32).to(device)
-
-y_tensor = y_tensor.reshape(-1,1)
-
-X_train, X_val , y_train , y_val = train_test_split(X_tensor, y_tensor, test_size=0.4, random_state=42)
-X_val , X_test , y_val, y_test = train_test_split(X_val,y_val, test_size=0.5, random_state=42)
-
-
-X_train = torch.from_numpy(scale.fit_transform(X_train)).type(torch.float32).to(device)
-X_test = torch.from_numpy(scale.transform(X_test)).type(torch.float32).to(device)
-X_val = torch.from_numpy(scale.transform(X_val)).type(torch.float32).to(device)
-
-train_data = TensorDataset(X_train, y_train)
-val_data = TensorDataset(X_val, y_val)
-test_data = TensorDataset(X_test, y_test)
-
-val_loader = DataLoader(val_data, batch_size=8,shuffle=True)
-train_loader = DataLoader(train_data, batch_size=8,shuffle=True)
-test_loader = DataLoader(test_data, batch_size=8,shuffle=True)
-
-
 
 model = MyModel().to(device)
 loss_function = nn.BCEWithLogitsLoss()
