064e1953c07bffc628c5d6d74ef5b326c6c13d5c
BenUsmane <syllaousmane279@gmail.com>
2026-02-24 01:36:05 +0800

    controller added

---
+++ React/server/controllers/flask.js
@@ -0,0 +1,23 @@
+const expresss = require("express")
+
+const postFlaskData = async (data) => {
+    const data = req.body
+    try{
+        const response = await fetch ("http://localhost:8000/predict", {
+            method : 'POST',
+            headers :{
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(data)
+        })
+            const result = await response.json()
+            res.json(result)
+            }
+
+     catch (error){
+        console.error('Error:', error)
+        res.status(500).json({ error: 'An error occurred' })
+    }
+}
+
+module.exports = postFlaskData
--- React/server/routes/flask_request.js
+++ React/server/routes/flask_request.js
@@ -1,23 +1,9 @@
-express = require('express');
+const postFlaskData = require('../controllers/flask');
+
+const express = require('express');
+
 const router = express.Router()
 
-router.post('/predict', async (req, res) => {
-    const data = req.body
-    try{
-        const response = await fetch ("http://localhost:8000/predict", {
-            method : 'POST',
-            headers :{
-                'content-type' : 'application/json'
-            },
-            body : JSON.stringify(data)
-        })
-            const result = await response.json()
-            res.json(result)
-            }
+router.post("/predict", postFlaskData)
 
-     catch (error){
-        console.error('Error:', error)
-        res.status(500).json({ error: 'An error occurred' })
-    }})
-
-    module.exports = router
+module.exports = router
